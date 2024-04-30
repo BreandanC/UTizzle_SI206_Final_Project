7a23ba1f37b81667f92d23ab6825cfa6074cd789
Breandan Cullen <157727075+BreandanC@users.noreply.github.com>
2024-04-30 16:12:02 -0400

    Add files via upload

--- plots.py
+++ plots.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 'Calculate Average Slope of Graph == Increase in Walkability per Increase in Population'
 
 # Get pop data
-conn = sql.connect('us_cities.db')
+conn = sql.connect('NA Cities.db')
 curr = conn.cursor()
 
 # Query the database
@@ -21,7 +21,7 @@ conn.commit()
 conn.close()
 
 # Get walk data 
-conn = sql.connect('walkability_scores.db')
+conn = sql.connect('NA Cities.db')
 curr = conn.cursor()
 
 # Query the database
@@ -49,15 +49,10 @@ plt.ylabel('Score')
 plt.grid(True)
 
 # Show the plot
-plt.show()
+# plt.show()
 
 score_per_pop = sum(scores) / sum(populations) * 1_000_000
 # print(score_per_pop)
-
-with open('results.txt', 'w') as f:
-    f.write("Score Increase per 1,000,000 Population:\n")
-    f.write(str(score_per_pop))
-
 
 
 # Now, create a histogram with Matplotlib
@@ -69,4 +64,43 @@ plt.xlabel('Population')  # Add an x-label
 plt.ylabel('Number of Cities')  # Add a y-label
 
 # Show the plot
-plt.show()
+# plt.show()
+
+
+
+conn = sql.connect('NA Cities.db')
+curr = conn.cursor()
+
+# Select data from each table where the city_id matches
+query = '''
+SELECT cd.population, aq.value
+FROM city_details AS cd
+JOIN AirQuality AS aq
+ON ABS(cd.latitude - aq.latitude) < 0.1
+AND ABS(cd.longitude - aq.longitude) < 0.1
+'''
+curr.execute(query)
+results = curr.fetchall()
+# for result in results:
+#     print(result)
+# Results is a list of tuples, where each tuple is (population, pm25)
+
+# Calculating the average air pollution per population.
+list_to_average = []
+for pop, part in results:
+    if part != 0:
+        list_to_average.append(pop / part)
+
+average_part_per_pop = (sum(list_to_average) / len(list_to_average)) / 1_000_000
+print(f"Average Increase in Particulate Matter (with size greater than or equal to 2.5) per 1,000,000 Increase in Population: {average_part_per_pop}")
+
+conn.close()
+
+
+
+with open('results.txt', 'w') as f:
+    f.write("Score Increase per 1,000,000 Population:\n")
+    f.write(str(score_per_pop))
+    f.write("\n")
+    f.write("Average Particulate Matter (with size greater than or equal to 2.5) per 1,000,000 Population:\n")
+    f.write(str(average_part_per_pop))
